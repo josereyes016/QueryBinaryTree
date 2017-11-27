9648005e2ccaaa89ba302d753fe57d039856d986
Jose <codesmash50@gmail.com>
2017-11-26 22:04:07 -0500

    Query BinaryTree using Extension Methods

--- QueryBinaryTree/Program.cs
+++ QueryBinaryTree/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using BinaryTree;
 
 namespace QueryBinaryTree
 {
@@ -9,7 +10,41 @@ namespace QueryBinaryTree
     {
         static void doWork()
         {
-            // TODO:
+            // Construct and Populate an instance of the BinaryTree class
+            Tree<Employee> empTree = new Tree<Employee>(
+                new Employee { Id = 1, FirstName = "Kim", LastName = "Amercrombire", Department = "IT" });
+
+            empTree.Insert(new Employee { Id = 2, FirstName = "Jeff", LastName = "Hay", Department = "Marketing" });
+            empTree.Insert(new Employee { Id = 4, FirstName = "Charlie", LastName = "Herb", Department = "IT" });
+            empTree.Insert(new Employee { Id = 6, FirstName = "Chris", LastName = "Preston", Department = "Sales" });
+            empTree.Insert(new Employee { Id = 3, FirstName = "Dave", LastName = "Barnett", Department = "Sales" });
+            empTree.Insert(new Employee { Id = 5, FirstName = "Tim", LastName = "Litton", Department = "Marketing" });
+
+            Console.WriteLine("List of departments");
+            var depts = empTree.Select(d => d.Department).Distinct();
+            foreach (var dept in depts)
+            {
+                Console.WriteLine("Department: {0}", dept);
+            }
+
+            Console.WriteLine("\nEmployees in the IT department");
+            var ITEmployees = empTree.Where(e => String.Equals(e.Department, "IT")).Select(emp => emp);
+            foreach (var emp in ITEmployees)
+            {
+                Console.WriteLine(emp);
+            }
+
+            Console.WriteLine("\nAll employees grouped by department");
+            var employeesByDept = empTree.GroupBy(e => e.Department);
+            foreach (var dept in employeesByDept)
+            {
+                Console.WriteLine("Department: {0}", dept.Key);
+                foreach (var emp in dept)
+                {
+                    Console.WriteLine("\t{0} {1}", emp.FirstName, emp.LastName);
+                }
+            }
+
         }
 
         static void Main()
